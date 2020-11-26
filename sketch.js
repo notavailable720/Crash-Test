1a95979fdcd81458853f73e680266fdb9893b705
notavailable720 <72535376+notavailable720@users.noreply.github.com>
2020-11-25 21:00:58 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,19 +3,21 @@ var speed, weight;
 
 function setup() {
   createCanvas(1600,400);
-  createSprite(400, 200, 50, 50);
+  //createSprite(400, 200, 50, 50);
 
   speed = random(55, 90);
   weight = random(400, 1500);
 
   car = createSprite(50, 200, 50, 50);
-  wall = createSprite(1500, 200, 60, 800);
+  wall = createSprite(1200, 200, 60, 300);
 }
 
 function draw() {
   background(255,255,255);  
 
   car.velocityX = speed;
+
+  collision();
 
   drawSprites();
 }
@@ -24,13 +26,13 @@ function collision() {
   if(isTouching(car, wall)) {
     var damage = (0.5 * weight * speed * speed) / 22500
     car.velocityX = 0
-  }
 
-  if(damage < 100) {
-    car.style.color = "green";
-  } else if(100 < damage < 180) {
-    car.style.color = "yellow"
-  } else if(damage > 180) {
-    car.style.color = "red";
+    if(damage < 100) {
+      car.shapeColor = "green";
+    } else if(100 < damage < 180) {
+      car.shapeColor = "yellow"
+    } else if(damage > 180) {
+      car.shapeColor = "red";
+    }
   }
 }
